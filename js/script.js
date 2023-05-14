58c9221cf5ab28ff4114aee574f5a3514c884a87
viren <code.viren@gmail.com>
2023-05-14 10:40:36 +0530

    developer details changes

--- js/script.js
+++ js/script.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Frontend Developer"],
+        strings: ["Frontend Developer", "React Js Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
